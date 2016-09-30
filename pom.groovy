2fa06583aa9a80fa06102749bcc73bb6accd5a6f
luismoramedina <luismoramedina@gmail.com>
2016-09-30 13:33:23 +0200

    Initial commit

---
+++ pom.groovy
@@ -0,0 +1,42 @@
+project {
+  modelVersion '4.0.0'
+  parent {
+    groupId 'org.springframework.boot'
+    artifactId 'spring-boot-starter-parent'
+    version '1.4.1.RELEASE'
+    relativePath ''
+  }
+  groupId 'com.github.luismoramedina'
+  artifactId 'groovy-pom'
+  version '0.0.1-SNAPSHOT'
+  name 'groovy-pom'
+  description 'Demo project for Spring Boot'
+  properties {
+    'project.reporting.outputEncoding' 'UTF-8'
+    'java.version' '1.8'
+    'project.build.sourceEncoding' 'UTF-8'
+  }
+  dependencies {
+    dependency {
+      groupId 'org.springframework.boot'
+      artifactId 'spring-boot-starter-web'
+    }
+    dependency {
+      groupId 'org.springframework.boot'
+      artifactId 'spring-boot-starter-test'
+      scope 'test'
+    }
+  }
+  build {
+    plugins {
+      plugin {
+        groupId 'org.springframework.boot'
+        artifactId 'spring-boot-maven-plugin'
+        configuration {
+          mainClass 'com.example.GroovyPomApplication'
+          layout 'zip'
+        }
+      }
+    }
+  }
+}
---
+++ src/main/java/com/github/luismoraedina/GroovyPomApplication.java
@@ -0,0 +1,12 @@
+package com.github.luismoraedina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyPomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyPomApplication.class, args);
+	}
+}
